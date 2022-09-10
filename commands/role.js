cf1204d374807cabc71bd75c124b6d27d00180b3
cmalucky <101647710+cmalucky@users.noreply.github.com>
2022-09-10 18:23:35 +0700

    Update role.js

--- commands/role.js
+++ commands/role.js
@@ -1,8 +1,7 @@
 module.exports ={
     name:"role",
     code:`
-    $deletecommand
-  $onlyPerms[admin;manageserver;$deletecommand <a:XGIF:1015223551957143583>  Bạn không có quyền sử dụng lệnh này!]
+    $onlyPerms[admin;manageserver;$deletecommand <a:XGIF:1015223551957143583>  Bạn không có quyền sử dụng lệnh này!]
 $color[843da4]
 $title[<a:rainbowww:1017314615853600779> __Gọi đồ ăn__ <a:rainbowww:1017314615853600779> ]
 $description[<a:staring:1007489309869346826> **Bạn muốn gọi đồ ăn gì nào ✧⁠◝⁠(⁠⁰⁠▿⁠⁰⁠)⁠◜⁠✧ **
@@ -14,9 +13,13 @@ __-----------------------------------------------------__
 __-----------------------------------------------------__
 <a:zz_pencil:1015628425802825800> *Sẽ cập nhật thêm các đồ ăn sau*
 ]
-$addButton[1;Xóa tất cả những thứ đã chọn;primary;role;no;<a:XGIF:1015223551957143583>]
-$addButton[1;Ice cream;primary;icecream;no;<:ph_icecream:1017313182215307264>]
+
+$addButton[2;Bỏ chọn món Ice cream;danger;role3;no;<a:XGIF:1015223551957143583>]
+$addButton[2;Bỏ chọn món Cake;danger;role2;no;<a:XGIF:1015223551957143583>]
+$addButton[2;Bỏ chọn món Coffee;danger;role1;no;<a:XGIF:1015223551957143583>]
+$addButton[1;Ice cream;primary;icecream;no;<a:ph_icecream:1017313182215307264>]
 $addButton[1;Cake;primary;cake;no;<a:cake2:1017312762893959209>]
-$addButton[1;Coffee;primary;coffee;no;<:Cafe:1017312614881185792>]
+$addButton[1;Coffee;primary;coffee;no;<a:Cafe:1017312614881185792>]
+$image[https://media.discordapp.net/attachments/901076082323763230/1017689972360347659/SPOILER_20220909_135548.gif]
   `
 }
