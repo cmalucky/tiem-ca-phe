a098e6f1e2eb3c94ab2ddef4447e4878109c0347
cmalucky <101647710+cmalucky@users.noreply.github.com>
2022-09-10 17:18:31 +0700

    Update rule.js

--- commands/rule.js
+++ commands/rule.js
@@ -7,28 +7,28 @@ module.exports ={
   $image[https://media.discordapp.net/attachments/901076082323763230/1017743623179227176/SPOILER_20220909_172835.gif]
   $title[<a:wing:1010080731407589428> <a:letterr:999152044332032090> <a:letteru:999152246262607892> <a:letterl:999151702232023131> <a:lettere:999151178971619328> <a:wing2:1010080773568737331>ㅤ]
   $description[> **__❖ 1. Hòa đồng.__**
-  > - Mọi người trong server hãy cố giữ thái độ vui vẻ, hòa đồng với nhau góp phần xây dựng một cộng đồng clean nhất quả đất.
-  > - Những động thái xúc phạm người khác, toxic quá mức, đe dọa, bội nhọ các bộ phận quản lí server sẽ bị xử phạt theo mức độ !
+ > - Mọi người trong server hãy cố giữ thái độ vui vẻ, hòa đồng với nhau góp phần xây dựng một cộng đồng clean nhất quả đất.
+ > - Những động thái xúc phạm người khác, toxic quá mức, đe dọa, bội nhọ các bộ phận quản lí server sẽ bị xử phạt theo mức độ !
   .
-  > **__❖ 2. Chat / Media / Bot Channels.__**
-  > - **Chat** : Không dùng các từ ngữ xúc phạm thái quá với mọi hình thức. [ không dùng form chữ, viết tắt của các từ ngữ bad word, ... để trốn lách luật. ]. Các ảnh hoặc video đăng ở kênh chat hãy xóa sau 10s...
-  > - **Media** : Những kênh chat ảnh, video, meme, ... không chat bằng chữ. ( chữ gắn liền với ảnh hoặc video thìy được )
-  > - **Bot channels** : Các kênh chat bot như owo, zerotwo, bot game, bot nhạc được phân kênh rõ ràng không dùng bot sai chỗ hoặc dùng để quấy phá người khác.
+ > **__❖ 2. Chat / Media / Bot Channels.__**
+ > - **Chat** : Không dùng các từ ngữ xúc phạm thái quá với mọi hình thức. [ không dùng form chữ, viết tắt của các từ ngữ bad word, ... để trốn lách luật. ]. Các ảnh hoặc video đăng ở kênh chat hãy xóa sau 10s...
+ > - **Media** : Những kênh chat ảnh, video, meme, ... không chat bằng chữ. ( chữ gắn liền với ảnh hoặc video thìy được )
+ > - **Bot channels** : Các kênh chat bot như owo, zerotwo, bot game, bot nhạc được phân kênh rõ ràng không dùng bot sai chỗ hoặc dùng để quấy phá người khác.
   .
-  > **__❖ 3. Spam & Scam.__**
-  > - **Spam** : Gửi một dạng gửi nhiều tin nhắn cùng một lúc hoặc một tin nhắn nhưng đúp kí tự / nhiều dòng, gây ảnh hưởng rất nhiều đến người khác. Cho nên không được gửi nhiều tin nhắn spam ở mọi kênh trong server!
-  > - **Scam** : Các hành vi lừa đảo gây thiệt hại cho người khác là tuyệt đối cấm !
+ > **__❖ 3. Spam & Scam.__**
+ > - **Spam** : Gửi một dạng gửi nhiều tin nhắn cùng một lúc hoặc một tin nhắn nhưng đúp kí tự / nhiều dòng, gây ảnh hưởng rất nhiều đến người khác. Cho nên không được gửi nhiều tin nhắn spam ở mọi kênh trong server!
+ > - **Scam** : Các hành vi lừa đảo gây thiệt hại cho người khác là tuyệt đối cấm !
   .
-  > **__❖ 4. Nội dung nhạy cảm.__**
-  > - Nội dung nhạy cảm [NSFW] như: bạo lực, ấu dâm, tình dục, máu me, giết chóc, ... sẽ không phù hợp cho các bạn dưới 18 tuổi ở server. Nên sẽ bị cấm ở đây!
+ > **__❖ 4. Nội dung nhạy cảm.__**
+ > - Nội dung nhạy cảm [NSFW] như: bạo lực, ấu dâm, tình dục, máu me, giết chóc, ... sẽ không phù hợp cho các bạn dưới 18 tuổi ở server. Nên sẽ bị cấm ở đây!
   .
-  > **__❖ 5. Rối loạn trật tự.__**
-  > - Các hành vi gây ảnh hưởng, rối loạn trật tự của server như : Tokens qua ngôn ngữ lập trình, Webhooks, Raid Bots. Cũng như các hành vi tự phát nhằm gây thù oán, chiến tranh sẽ bị xử phạt cho tất cả trường hợp.
+ > **__❖ 5. Rối loạn trật tự.__**
+ > - Các hành vi gây ảnh hưởng, rối loạn trật tự của server như : Tokens qua ngôn ngữ lập trình, Webhooks, Raid Bots. Cũng như các hành vi tự phát nhằm gây thù oán, chiến tranh sẽ bị xử phạt cho tất cả trường hợp.
   .
-  > **__❖ 6. ToS__**
-  > Hãy tuân thủ luật của Discord:
-  > https://discord.com/terms
-  > https://discord.com/privacy
+ > **__❖ 6. ToS__**
+ > Hãy tuân thủ luật của Discord:
+ > https://discord.com/terms
+ > https://discord.com/privacy
   .
   *❖ Việc không tuân thủ luật sẽ dẫn đến việc bị xử phạt theo mức độ.*]
   `
