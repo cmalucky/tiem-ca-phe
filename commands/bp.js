1f1d5e86b720434d4b16a1919ffcc18afb9494c6
cmalucky <101647710+cmalucky@users.noreply.github.com>
2022-09-11 16:09:40 +0700

    Update bp.js

--- commands/bp.js
+++ commands/bp.js
@@ -1,1 +1,19 @@
-
+module.exports ={
+  name: "bp",
+  code:`
+  $onlyPerms[admin;manageserver;$deletecommand <a:XGIF:1015223551957143583>  Bạn không có quyền sử dụng lệnh này!]
+$color[763819]
+$title[<​a:wing:1010080731407589428> <​a:lettere:999151178971619328> <​a:letterd:999151116078026762> <​a:letteri:999151415643615374> <​a:lettert:999152193573756969> <​a:stttar:1010330887218532433> <​a:letterr:999152044332032090> <​a:lettero:999151863960182795> <​a:lettero:999151863960182795> <​a:letterm:999151760155365417> <​a:letters:999152147344150669> <​a:wing2:1010080773568737331> ]
+$description[**__Sử dụng bot @PartyBeast#0187 __**
+**Các câu lệnh:**
+/lock : để khoá phòng của bạn lại
+/unlock : để mở phòng lúc khoá
+/rename (to) : để đổi tên phòng
+/ban : chỉ định một người cấm vào phòng của bạn
+/limit (to) : để giới hạn người vào
+/invite (user) : thêm người đó vào khi phòng đang khoá
+/kick : để đá người đó ra phòng của bạn
+/owner : chuyển chủ phòng cho người khác]
+$footer[1;by cmalucky#7696;$userAvatar]
+`
+}
