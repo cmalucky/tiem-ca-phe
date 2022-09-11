8f9ca4dbc8fc5e903369e136a30c52dc988af274
cmalucky <101647710+cmalucky@users.noreply.github.com>
2022-09-11 16:11:58 +0700

    Update bp.js

--- commands/bp.js
+++ commands/bp.js
@@ -14,6 +14,6 @@ $description[**__Sử dụng bot @PartyBeast#0187 __**
 /invite (user) : thêm người đó vào khi phòng đang khoá
 /kick : để đá người đó ra phòng của bạn
 /owner : chuyển chủ phòng cho người khác]
-$footer[1;by cmalucky#7696;$userAvatar]
+$footer[1;by cmalucky#7696;$userAvatar[$authorID]]
 `
 }
